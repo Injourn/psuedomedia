b622bfce53f35ade3246d628b790f63d7cf9c71e
Injourn <thatguywhowas@live.com>
2022-06-17 23:14:40 -0700

    Fixed a couple of issues with the navbar

--- src/NavBar.js
+++ src/NavBar.js
@@ -12,14 +12,14 @@ class NavBar extends React.Component{
         if(props.active){
             return(
                 <li className="nav-item active">
-                    <RouteLink to={props.LinkRef}>{props.LinkName} <span className="sr-only">(current)</span></RouteLink>
+                    <RouteLink className='nav-link' to={props.LinkRef}>{props.LinkName} <span className="sr-only">(current)</span></RouteLink>
                 </li>
             );
         }
         else{
             return(
                 <li className="nav-item">
-                    <RouteLink to={props.LinkRef}>{props.LinkName}</RouteLink>
+                    <RouteLink className='nav-link' to={props.LinkRef}>{props.LinkName}</RouteLink>
                 </li>
             );
         }
@@ -30,7 +30,7 @@ class NavBar extends React.Component{
         return (
           <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <this.Brand/>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
