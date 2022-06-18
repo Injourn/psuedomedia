eace46fa454bcabe7160810eb48f28c047fbf75b
Injourn <thatguywhowas@live.com>
2022-06-17 22:51:17 -0700

    Changed incoreect class name

--- src/SideBar.js
+++ src/SideBar.js
@@ -10,12 +10,12 @@ class SideBar extends React.Component{
         return (
             <div className='col-lg-3 d-none d-lg-block my-5' id="sticky-sidebar">
                 <div className='sticky-top'>
-                    <ul class="nav flex-column nav-pills menu-sidebar text-start" data-mdb-allow-hashes="true">                   
-                        <li class="nav-item">
-                            <a class="nav-link" href="#section-introduction">View All Posts</a>
+                    <ul className="nav flex-column nav-pills menu-sidebar text-start" data-mdb-allow-hashes="true">                   
+                        <li className="nav-item">
+                            <a className="nav-link" href="#section-introduction">View All Posts</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#section-basic-example">Only View Friends' Posts</a>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#section-basic-example">Only View Friends' Posts</a>
                         </li>
                     </ul>
                 </div>
