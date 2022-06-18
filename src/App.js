64f8031592e7aff015dd7de0e4321fff28da0aff
Injourn <thatguywhowas@live.com>
2022-06-18 14:55:21 -0700

    Added registe screen.

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import {
   Route,
   Link
 } from "react-router-dom";
+import Register from './Register';
 
 function App() {
   return (
@@ -18,7 +19,7 @@ function App() {
           <Routes>
             <Route path="/" element={<MainPage />} />
             <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<h1>unavailable</h1>}  />
+            <Route path="/register" element={<Register />}  />
           </Routes>
       </div>
     </Router>
